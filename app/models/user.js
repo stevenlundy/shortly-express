439898708eaae9e973c78f91ee1b923b3309c202
stevenlundy <steven.lundy@gmail.com>
2015-09-14 16:23:22 -0700

    Wrote user model for bookshelf and updated relations between models

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,21 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  sessions: function(){
+    return this.hasMany(Session);
+  },
+  links: function(){
+    return this.hasMany(Link);
+  },
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      var salt = bcrypt.genSaltSync(16);
+      model.set('salt', salt);
+      model.set('password', bcrypt.hashSync(options.password, salt));
+    });
+  }
 });
 
 module.exports = User;
